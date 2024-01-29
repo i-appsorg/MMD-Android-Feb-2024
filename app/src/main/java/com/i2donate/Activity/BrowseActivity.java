24ba9ce6cd5d7179f92ab446324d768a00d51791
tisha <tisha.kakadiya@viprak.com>
2024-01-29 11:30:54 +0530

    Browser Screen change

--- app/src/main/java/com/i2donate/Activity/BrowseActivity.java
+++ app/src/main/java/com/i2donate/Activity/BrowseActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -20,26 +21,54 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.gson.JsonObject;
 import com.i2donate.Adapter.CategorylistAdapter;
 import com.i2donate.CommonActivity.CommonMenuActivity;
+import com.i2donate.Commonmethod.SearchableSpinner;
 import com.i2donate.Model.ChangeActivity;
+import com.i2donate.Model.CurrencyBean;
 import com.i2donate.Model.Selected;
 import com.i2donate.R;
+import com.i2donate.RetrofitAPI.ApiClient;
+import com.i2donate.RetrofitAPI.ApiInterface;
 import com.i2donate.Session.IDonateSharedPreference;
 import com.i2donate.Session.SessionManager;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class BrowseActivity extends CommonMenuActivity {
     private String TAG = "BrowseActivity";
     Toolbar toolbar;
     TextView /*better_tv,*/ advance_search_tv;
     SessionManager session;
-    RelativeLayout name_relative_layout, united_state_location_relative, type_relative_layout, international_layout;
-//    ImageView name_relative_layout, united_state_location_relative, type_relative_layout, international_layout;
+//    RelativeLayout name_relative_layout, united_state_location_relative, type_relative_layout, international_layout;
+    ImageView name_relative_layout, united_state_location_relative, type_relative_layout, international_layout;
     IDonateSharedPreference iDonateSharedPreference;
     static ArrayList<String> listOfdate = new ArrayList<>();
     static ArrayList<String> arraychecked_item = new ArrayList<>();
+
+
+
+    SearchableSpinner country_spinner;
+
+    JSONArray jsonArray;
+    ArrayList<String> country = new ArrayList<>();
+    ArrayList<String> country_name_id = new ArrayList<String>();
+    List<CurrencyBean> country_name_list = new ArrayList<CurrencyBean>();
+    private final static String API_KEY = "";
+    ApiInterface apiService;
+    ArrayList<CurrencyBean> country1 = new ArrayList<CurrencyBean>();
+    String country_symbol = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +80,69 @@ public class BrowseActivity extends CommonMenuActivity {
         toolbar = findViewById(R.id.commonMenuActivityToolbar);
 
         setSelected(Selected.Browse);
+        CountryAPI();
+
         init();
         listener();
 
     }
+
+    private void CountryAPI() {
+        country.clear();
+        country_name_id.clear();
+        country_name_list.clear();
+        JsonObject jsonObject1 = new JsonObject();
+        jsonObject1.addProperty("email", API_KEY);
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<JsonObject> call = apiService.countryAPI(jsonObject1);
+        Log.e(TAG, "CountryAPI: " + apiService);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                Log.e(TAG, "123" + response);
+                try {
+                    JSONObject jsonObject = new JSONObject(String.valueOf(response.body()));
+                    Log.e(TAG, "" + jsonObject.toString());
+                    String message = jsonObject.getString("message");
+                    Log.e(TAG, "" + message.toString());
+                    if (jsonObject.getString("status").equals("1")) {
+                        String data = jsonObject.getString("data");
+                        jsonArray = new JSONArray(data);
+                        Log.e(TAG, "" + jsonArray.toString());
+                        HashMap<String, String> map = new HashMap<>();
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+
+                            JSONObject countryobject = jsonArray.getJSONObject(i);
+                            Log.e(TAG, "" + countryobject.toString());
+                            CurrencyBean ctb = new CurrencyBean();
+                            country.add(countryobject.getString("name"));
+                            ctb.setCurrency_id(countryobject.getString("sortname"));
+                            ctb.setCurrency_name(countryobject.getString("name"));
+                            country_name_id.add(countryobject.getString("sortname"));
+                            country_name_list.add(ctb);
+                            country1.add(ctb);
+                            if (countryobject.getString("name").equalsIgnoreCase("United States")) {
+
+                                country_spinner.setDefaultText(countryobject.getString("name"));
+                                country_symbol = countryobject.getString("sortname");
+                            }
+                        }
+                        country_spinner.setData(country);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+
 
 //    private void notification() {
 //        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
@@ -90,6 +178,8 @@ public class BrowseActivity extends CommonMenuActivity {
         iDonateSharedPreference.setadvance(getApplicationContext(), "0");
         iDonateSharedPreference.setselectedtypedata(getApplicationContext(), listOfdate);
         iDonateSharedPreference.setselected_iem_list(getApplicationContext(), arraychecked_item);
+        country_spinner = (SearchableSpinner) findViewById(R.id.spin_country);
+
     }
 
     public String getDeviceUniqueID(Activity activity) {
@@ -148,6 +238,28 @@ public class BrowseActivity extends CommonMenuActivity {
                 ChangeActivity.changeActivityData(BrowseActivity.this, InternationalCharitiesActivity.class, "0");
             }
         });
+
+        country_spinner.setSelectionListener(new SearchableSpinner.OnSelectionListener() {
+            @Override
+            public void onSelect(int spinnerId, int i, String value) {
+                Log.e("Select2", "Position : " + i + " : Value : " + value + " : " + spinnerId);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(country_spinner.getWindowToken(),
+                        InputMethodManager.RESULT_UNCHANGED_SHOWN);
+                for (int i1 = 0; i1 < jsonArray.length(); i1++) {
+                    JSONObject countryobject = null;
+                    try {
+                        countryobject = jsonArray.getJSONObject(i1);
+                        if (countryobject.getString("name").equals(value)) {
+                            country_symbol = countryobject.getString("sortname");
+                            Log.e(TAG, country_symbol);
+                        }
+                    } catch (JSONException e) {
+                    }
+                }
+            }
+        });
+
     }
 
     private void LoginDialog() {
